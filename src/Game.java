c47d8c83e5257d1bd1abed76d2639fc9b66c74bd
Sergio Dobrianskiy <sergio.dobrianskiy@studio.unibo.it>
2023-03-16 22:41:43 +0100

    Aggiunta creazione Window

--- src/Game.java
+++ src/Game.java
@@ -6,11 +6,24 @@ public class Game extends Canvas implements Runnable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	public static int WIDTH = 800, HEIGHT = 608;
+	public String title = "Zombie Game";
+	
+	
+	public Game() {
+		//Construct
+		new Window(WIDTH, HEIGHT, title, this);
+	}
+	
 
 	@Override
 	public void run() {
 		
 		
+	}
+	
+	public static void main(String args[]) {
+		new Game();
 	}
 
 }
