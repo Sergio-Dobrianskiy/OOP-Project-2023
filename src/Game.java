e3bedc1f8e225007de4572ac27306d7ed3c612c7
Sergio Dobrianskiy <sergio.dobrianskiy@studio.unibo.it>
2023-03-16 22:13:09 +0100

    crato branch per Wizard Game

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void run() {
+		
+		
+	}
+
+}
